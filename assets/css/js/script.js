e2089399f90a1991892bb5127b4500177bf33a9d
Devon <devondgray28@gmail.com>
2022-05-22 15:16:38 -0500

    criteria variables

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,7 +1,12 @@
 // Assignment code here
 
+//Create diiferent type of criteria for the generater to pick from. 
+var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "*", "(", ")"]; 
 
-// Get references to the #generate element
+// reference to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
